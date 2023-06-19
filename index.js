569f8664bead2d39a5be20d3071a25ffe864b18f
janindu-kulathilka <86515184+janindu-kulathilaka@users.noreply.github.com>
2023-06-19 14:01:21 +0530

    do the changes

--- index.js
+++ index.js
@@ -1,76 +1,89 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3001;
 
-const QUESTIONS = [
-  {
+const USERS = [];
+
+app.use(express.json());
+
+const QUESTIONS = [{
     title: "Two states",
-    description: "Given an array, return the maximum of the array?",
-    testCases: [
-      {
+    description: "Given an array , return the maximum of the array?",
+    testCases: [{
         input: "[1,2,3,4,5]",
-        output: "5",
-      },
-    ],
-  },
-];
+        output: "5"
+    }]
+}];
+
 
 const SUBMISSION = [
-  {
-    userId: "1",
-    questionId: "1",
-    code: "function max(arr) { return Math.max(...arr) }",
-    status: "accepted",
-  },
-  {
-    userId: "2",
-    questionId: "2",
-    code: "function min(arr) { return Math.max(...arr) }",
-    status: "rejected",
-  },
-];
 
-app.post("/signup", function (req, res) {
-  //Add logic to decode body
-  //body should have email and password
+]
 
-  //Store email and password (as is for now) in the USERS array above (only if the user with the given email doesn't exit)
-
-  //return back 200 status code to the client
-
-  res.send("Hello World! from signup");
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './signup.html'));
 });
 
-app.post("/login", function (req, res) {
-  //Add logic to decode body
-  //body should have email and password
+app.post('/signup', function(req, res) {
+  const { email, password } = req.body;
 
-  // check if the user with the given email exists in the USERS array
-  // also ensure that the password is the same
+  const isUserExist = USERS.find(user => user.email === email);
 
-  // if the password is the same, return back 200 status code to the client
-  // also send back a token (any random string will do for now)
-  // if the password is not the same,  return back 401 status code to the client
+  if (isUserExist) {
+    return res.status(409).send('User already exists');
+  }
 
-  res.send("Hello World! from login");
+  const newUser = {
+    email,
+    password
+  };
+
+  USERS.push(newUser);
+
+  res.status(200).send('Sign up successful');
+  res.status(200).send(USERS);
+  
+})
+
+app.post('/login', function(req, res) {
+  // Add logic to decode body
+  // body should have email and password
+
+  // Check if the user with the given email exists in the USERS array
+  // Also ensure that the password is the same
+
+
+  // If the password is the same, return back 200 status code to the client
+  // Also send back a token (any random string will do for now)
+  // If the password is not the same, return back 401 status code to the client
+
+
+  res.send('Hello World from route 2!')
+})
+
+app.get('/questions', function(req, res) {
+
+  //return the user all the questions in the QUESTIONS array
+  res.send("Hello World from route 3!")
+})
+
+app.get("/submissions", function(req, res) {
+   // return the users submissions for this problem
+  res.send("Hello World from route 4!")
 });
 
-app.get("/questions", function (req, res) {
-  //return the user all questions in the QUESTIONS array
-  res.send("Hello World! from questions");
+
+app.post("/submissions", function(req, res) {
+   // let the user submit a problem, randomly accept or reject the solution
+   // Store the submission in the SUBMISSION array above
+  res.send("Hello World from route 4!")
 });
 
-app.get("/submissions", function (req, res) {
-  // return the users submissions for this problem
-  res.send("Hello World! from submissions");
-});
+// leaving as hard todos
+// Create a route that lets an admin add a new problem
+// ensure that only admins can do that.
 
-app.post("/submissions", function (req, res) {
-  // let the user submit a problem, randomly accept or reject the solution
-  // store the submission in the SUBMISSION array above
-  res.send("Hello World! from submissions");
-});
-
-app.listen(port, function () {
-  console.log(`Example app listening on port ${port}`);
-});
+app.listen(port, function() {
+  console.log(`Example app listening on port ${port}`)
+})
